77e196d63a9c1c98f76daa712ac1bf735ad25230
xylonx <xylon-x@foxmail.com>
2021-10-10 19:28:01 +0800

    :building_construction: lark oauth redirect

--- SSO/controller/oauth_callback.go
+++ SSO/controller/oauth_callback.go
@@ -1,7 +1,37 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"net/http"
 
+	"github.com/UniqueStudio/UniqueSSO/pkg"
+	"github.com/UniqueStudio/UniqueSSO/util"
+	"github.com/gin-gonic/gin"
+)
+
+// for first login, append user info. otherwise just login
 func LarkOauthCallbackHandler(ctx *gin.Context) {
+	apmCtx, span := util.Tracer.Start(ctx.Request.Context(), "LarkOauthCallbackHandler")
+	defer span.End()
+
+	code, ok := ctx.GetQuery("code")
+	if !ok {
+		ctx.JSON(http.StatusBadGateway, pkg.InvalidRequest(errors.New("no code in redirect url")))
+		return
+	}
+	// service := ctx.Query("state")
+
+	userToken, err := util.GetLarkUserToken(apmCtx, code)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, pkg.InternalError(err))
+		return
+	}
+
+	larkInfo, err := util.GetLarkUserInfo(apmCtx, userToken)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, pkg.InternalError(err))
+		return
+	}
+
 
 }

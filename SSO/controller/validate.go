60d6135c46cfcb25104b10784d9217660f2399f0
xylonx <x956200361@gmail.com>
2021-10-12 12:25:18 +0000

    :sparkles: (Traefik) add traefik forward-auth

--- SSO/controller/validate.go
+++ SSO/controller/validate.go
@@ -1,7 +1,24 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/UniqueStudio/UniqueSSO/common"
+	"github.com/UniqueStudio/UniqueSSO/conf"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
 
 func TraefikAuthValidate(ctx *gin.Context) {
+	sess := sessions.Default(ctx)
 
+	uid, ok := sess.Get(common.SESSION_NAME_UID).(string)
+	if !ok {
+		ctx.Redirect(http.StatusFound, conf.SSOConf.Application.TraefikRedirectURI)
+		return
+	}
+
+	ctx.Writer.Header().Set("X-UID", uid)
+	ctx.Status(http.StatusOK)
+	return
 }
